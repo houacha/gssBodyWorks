6b01341a0a3cd2c5e80994ffc09bfb7b38c1cb10
houa cha <itshoua@gmail.com>
2021-09-16 18:34:32 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return <div className="App">
+    <Router>
+      <Switch>
+
+      </Switch>
+    </Router>
+  </div>;
+}
+
+export default App;
---
+++ src/Availability.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Availabilty.css";
+
+function Availability() {
+  return <div></div>;
+}
+
+export default Availability;
